b46fc82cb3b8fa7ad7307840e3f573331e1535ed
Jildert Venema <j.venema@ziuz.com>
2020-06-12 14:33:47 +0200

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,8 +21,9 @@ export default class Header extends Component {
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
+               <h1 className="responsive-headline">JILDERT.DEV</h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>
+                 I am a Computing Science student and frontend developer.
                </h3>
                <hr/>
                <ul className="social">
